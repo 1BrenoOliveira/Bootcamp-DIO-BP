313794635d890233a6f774a82efca23453917a9d
1BrenoOliveira <1brenomiranda@gmail.com>
2023-04-28 12:18:48 -0300

    adicionando desario 2 - estrutura mais basica de autenticação

---
+++ Desafio 002 - Spring security, web, swagger/UserManagement-SpringAuthentication/src/main/java/dev/breno/UserManagementSpringAuthentication/UserManagementSpringAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package dev.breno.UserManagementSpringAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementSpringAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementSpringAuthenticationApplication.class, args);
+	}
+
+}
---
+++ Desafio 002 - Spring security, web, swagger/UserManagement-SpringAuthentication/src/test/java/dev/breno/UserManagementSpringAuthentication/UserManagementSpringAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.breno.UserManagementSpringAuthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementSpringAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
